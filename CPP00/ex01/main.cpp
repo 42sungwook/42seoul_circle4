a2d8f253e4e3d2d0417510df6bcb808755518aa4
Sungwook Kim <sungwook@c1r4s5.42seoul.kr>
2023-07-01 21:04:24 +0900

    edit repeated phrases

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,5 +1,16 @@
 #include "PhoneBook.hpp"
 
+std::string getInput(const std::string& message, const std::string& errorMessage) {
+    std::string input;
+		
+    std::cout << BLUE_TEXT << message << RESET_TEXT;
+    if (!std::getline(std::cin, input) || input.empty()) {
+        std::cout << RED_TEXT << errorMessage << RESET_TEXT << std::endl;
+        return "";
+    }
+    return input;
+}
+
 int main() {
     PhoneBook phoneBook;
     std::string command;
@@ -8,57 +19,50 @@ int main() {
         std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
 
         if (!std::getline(std::cin, command)) {
-					  std::cout << "EXIT" << std::endl;
+            std::cout << "EXIT" << std::endl;
             break;
         }
+
         if (command == "ADD") {
             std::string firstName, lastName, nickname, phoneNumber, darkestSecret;
-            std::cout << BLUE_TEXT << "Enter First Name: " << RESET_TEXT;
-            if (!std::getline(std::cin, firstName) || firstName.empty()) {
-                std::cout << RED_TEXT << "First Name cannot be empty." << RESET_TEXT << std::endl;
-                continue;
-            }
-            std::cout << BLUE_TEXT << "Enter Last Name: " << RESET_TEXT;
-            if (!std::getline(std::cin, lastName) || lastName.empty()) {
-                std::cout << RED_TEXT << "Last Name cannot be empty." << RESET_TEXT << std::endl;
-                continue;
-            }
-            std::cout << BLUE_TEXT << "Enter Nickname: " << RESET_TEXT;
-            if (!std::getline(std::cin, nickname) || nickname.empty()) {
-                std::cout << RED_TEXT << "Nickname cannot be empty." << RESET_TEXT << std::endl;
-                continue;
-            }
-            std::cout << BLUE_TEXT << "Enter Phone Number: " << RESET_TEXT;
-            if (!std::getline(std::cin, phoneNumber) || phoneNumber.empty()) {
-                std::cout << RED_TEXT << "Phone Number cannot be empty." << RESET_TEXT << std::endl;
-                continue;
-            }
-            std::cout << BLUE_TEXT << "Enter Darkest Secret: " << RESET_TEXT;
-            if (!std::getline(std::cin, darkestSecret) || darkestSecret.empty()) {
-                std::cout << RED_TEXT << "Darkest Secret cannot be empty." << RESET_TEXT << std::endl;
-                continue;
-            }
+
+            firstName = getInput("Enter First Name: ", "First Name cannot be empty.");
+            if (firstName.empty()) continue;
+
+            lastName = getInput("Enter Last Name: ", "Last Name cannot be empty.");
+            if (lastName.empty()) continue;
+
+            nickname = getInput("Enter Nickname: ", "Nickname cannot be empty.");
+            if (nickname.empty()) continue;
+
+            phoneNumber = getInput("Enter Phone Number: ", "Phone Number cannot be empty.");
+            if (phoneNumber.empty()) continue;
+
+            darkestSecret = getInput("Enter Darkest Secret: ", "Darkest Secret cannot be empty.");
+            if (darkestSecret.empty()) continue;
+
             Contact newContact(firstName, lastName, nickname, phoneNumber, darkestSecret);
             phoneBook.addContact(newContact);
         }
-        else if (command == "SEARCH") {
-            phoneBook.displayContacts();
+				else if (command == "SEARCH") {
+						phoneBook.displayContacts();
 
-            int index;
-            std::cout << BLUE_TEXT << "Enter the index of the contact to display: " << RESET_TEXT;
-            if (!(std::cin >> index)) {
-								if (std::cin.eof()) {
-									std::cout << "EXIT" << std::endl;
-									break;
-								}
-                std::cout << RED_TEXT << "Invalid index." << RESET_TEXT << std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                continue;
-            }
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            phoneBook.searchContact(index - 1);
-        }
+						std::string indexStr;
+						int index;
+
+						std::cout << BLUE_TEXT << "Enter the index of the contact to display: " << RESET_TEXT;
+						if (!std::getline(std::cin, indexStr)) {
+								std::cout << RED_TEXT << "Invalid index." << RESET_TEXT << std::endl;
+								continue;
+						}
+						try {
+								index = std::stoi(indexStr);
+						} catch (const std::invalid_argument&) {
+								std::cout << RED_TEXT << "Invalid index." << RESET_TEXT << std::endl;
+								continue;
+						}
+						phoneBook.searchContact(index - 1);
+				}
         else if (command == "EXIT") {
             break;
         }
@@ -67,5 +71,6 @@ int main() {
                 std::cout << RED_TEXT << "Invalid command." << RESET_TEXT << std::endl;
         }
     }
+
     return 0;
 }
