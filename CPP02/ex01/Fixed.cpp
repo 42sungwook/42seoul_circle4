f585cecb44bb93c7ba95bfa12e8a8a26764a70be
Sungwook Kim <sungwook@c1r6s3.42seoul.kr>
2023-07-12 11:41:01 +0900

    add cpp02

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,64 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_value = value << _bits;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_value = (int)roundf(value * (1 << _bits));
+}
+
+Fixed::Fixed(const Fixed& src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_value = rhs.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float)_value / (1 << _bits);
+}
+
+int Fixed::toInt(void) const
+{
+	return _value >> _bits;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& rhs)
+{
+	os << rhs.toFloat();
+	return os;
+}
